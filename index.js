34ff2621cdd576119ee7a4507e571ebd874aa353
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-29 23:43:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  var number = a + b
+  return number
+}
+
+function subtract(b, a) {
+  var number = b - a 
+  return number
+}
+
+function multiply(a, b) {
+  var number = a * b 
+  return number 
+}
+
+function divide(a, b) {
+  var number = a / b 
+  return number  
+}
+
+function inc(n) {
+  var number = n += 1 
+  return number 
+}
+
+function dec(n) {
+  var number = n -= 1
+  return dec 
+}
+
+function makeInt(n) {
+  
+}
